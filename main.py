f11283fc42acacb08704dde8586e15fb5b463288
reonlll <twelve120901@icloud.com>
2025-06-15 15:00:18 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class MyClient(commands.Bot):
 
 intents = discord.Intents.default()
 intents.message_content = True
-
+intents.members = True
 bot = MyClient(command_prefix="!", intents=intents)
 
 # /残高
