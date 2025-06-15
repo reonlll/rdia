23d75706481855d45ade4dd12033ea3ccd57525f
reonlll <twelve120901@icloud.com>
2025-06-15 11:55:51 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import os
 import discord
 from discord.ext import commands
+
+# 環境変数からトークンを読み込む
+TOKEN = os.environ.get("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -14,4 +18,5 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("ぽん！")
 
-bot.run("ここにトークン")
+# トークン実行
+bot.run(TOKEN)
